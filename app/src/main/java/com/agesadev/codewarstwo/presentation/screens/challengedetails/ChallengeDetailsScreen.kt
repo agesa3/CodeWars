a1be9ab4ba0f2fbe096f3dde7a64fb171d086b23
agesa3 <collinsagesa2000@gmail.com>
2022-09-21 01:29:49 +0300

    able to display the markdown text

--- app/src/main/java/com/agesadev/codewarstwo/presentation/screens/challengedetails/ChallengeDetailsScreen.kt
+++ app/src/main/java/com/agesadev/codewarstwo/presentation/screens/challengedetails/ChallengeDetailsScreen.kt
@@ -2,12 +2,10 @@ package com.agesadev.codewarstwo.presentation.screens.challengedetails
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.R
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,6 +14,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.agesadev.codewarstwo.data.remote.dto.detail.ApprovedBy
 import com.agesadev.codewarstwo.data.remote.dto.detail.CreatedBy
@@ -24,6 +23,7 @@ import com.agesadev.codewarstwo.presentation.model.ChallengeDetails
 import com.agesadev.codewarstwo.presentation.screens.challengedetails.components.CardItem
 import com.agesadev.codewarstwo.presentation.screens.challengedetails.components.ChallengeTag
 import com.google.accompanist.flowlayout.FlowRow
+import dev.jeziellago.compose.markdowntext.MarkdownText
 
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -55,26 +55,26 @@ fun ChallengeDetailsScreen(challengeDetailsViewModel: ChallengeDetailsViewModel 
                     textAlign = TextAlign.Start,
                     overflow = TextOverflow.Ellipsis,
                 )
-                Spacer(modifier = Modifier.height(10.dp))
+                MySpacer(spacerHeight = 10)
                 Text(text = "Created By ${detailState.challenge?.createdBy?.creatorUsername}")
-                Spacer(modifier = Modifier.height(10.dp))
+                MySpacer(spacerHeight = 10)
                 Text(text = "Approved By ${detailState.challenge?.approvedBy?.usernameApproved}")
-                Spacer(modifier = Modifier.height(10.dp))
+                MySpacer(spacerHeight = 10)
                 Text(
                     text = "Description",
                     textAlign = TextAlign.Start,
                     color = Color.Black,
                     style = MaterialTheme.typography.h6
                 )
-                Text(text = "${detailState.challenge?.description}")
-                Spacer(modifier = Modifier.height(10.dp))
+                MyMarkDownText(markdown = "${detailState.challenge?.description}")
+                MySpacer(spacerHeight = 10)
                 Text(
                     text = "Tags",
                     textAlign = TextAlign.Start,
                     color = Color.Black,
                     style = MaterialTheme.typography.h6
                 )
-                Spacer(modifier = Modifier.height(10.dp))
+                MySpacer(spacerHeight = 10)
                 FlowRow(
                     mainAxisSpacing = 8.dp,
                     crossAxisSpacing = 8.dp,
@@ -87,7 +87,7 @@ fun ChallengeDetailsScreen(challengeDetailsViewModel: ChallengeDetailsViewModel 
                 }
                 Spacer(modifier = Modifier.height(10.dp))
                 Text(text = "Statistics")
-                Spacer(modifier = Modifier.height(10.dp))
+                MySpacer(spacerHeight = 10)
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -103,36 +103,20 @@ fun ChallengeDetailsScreen(challengeDetailsViewModel: ChallengeDetailsViewModel 
     }
 }
 
+@Composable
+fun MySpacer(spacerHeight: Int) {
+    Spacer(modifier = Modifier.height(spacerHeight.dp))
+}
 
-//@Preview
-//@Composable
-//fun ChallengeDetailsScreenPreview() {
-//    ChallengeDetailsScreen(deta = chllengeDetails)
-//}
-//
-//val chllengeDetails = ChallengeDetails(
-//    id = "5a01be8cffeeda001a0d7c94",
-//    name = "Sum Mixed Array",
-//    slug = "sum-mixed-array",
-//    description = "Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.",
-//    category = "algorithms",
-//    rank = Rank(
-//        rankId = 1,
-//        rankName = "-4 kyu",
-//        color = "#55d500"
-//    ),
-//    languages = listOf("JavaScript", "Python", "Ruby"),
-//    tags = listOf("Fundamentals", "Arrays", "Strings", "Numbers", "Data Types"),
-//    approvedBy = ApprovedBy(
-//        approvedByUrl = "55a70521798b14d4750000a4",
-//        usernameApproved = "jhoffner"
-//    ),
-//    publishedAt = "2017-11-22T17:51:42.000Z",
-//    totalAttempts = 0,
-//    totalCompleted = 0,
-//    totalStars = 0,
-//    createdBy = CreatedBy(
-//        creatorUsername = "jhoffner",
-//        createdByUrl = "https://www.codewars.com/users/jhoffner"
-//    ),
-//)
+@Composable
+fun MyMarkDownText(markdown: String) {
+    MarkdownText(
+        modifier = Modifier.padding(8.dp),
+        markdown = markdown,
+        textAlign = TextAlign.Start,
+        fontSize = 12.sp,
+        color = LocalContentColor.current,
+        maxLines = 3,
+        style = MaterialTheme.typography.overline,
+    )
+}
