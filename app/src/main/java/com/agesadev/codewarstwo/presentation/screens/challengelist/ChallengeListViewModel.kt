24bd6bfaafbbc78ee5f565744aed14da56fd5fa9
agesa3 <collinsagesa2000@gmail.com>
2022-09-23 14:22:51 +0300

    paginated ui change

--- app/src/main/java/com/agesadev/codewarstwo/presentation/screens/challengelist/ChallengeListViewModel.kt
+++ app/src/main/java/com/agesadev/codewarstwo/presentation/screens/challengelist/ChallengeListViewModel.kt
@@ -15,6 +15,7 @@ import com.agesadev.codewarstwo.util.Utils.TEST_USERNAME
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -24,32 +25,11 @@ class ChallengeListViewModel @Inject constructor(
     private val getCompletedChallengesUseCase: GetCompletedChallengesUseCase,
 ) : ViewModel() {
 
-    //    private val _completedChallenges = mutableStateOf(ChallengeListState())
-//    val completedChallenges: State<ChallengeListState> = _completedChallenges
-//
-//    init {
-//        getCompletedChallenges("matt c")
-//    }
-//
-//    private fun getCompletedChallenges(username: String) {
-//        viewModelScope.launch {
-//            getCompletedChallengesUseCase(username).cachedIn(viewModelScope)
-//                .collectLatest { pagingData ->
-//                    _completedChallenges.value = ChallengeListState(
-//                        challenges = pagingData.map { it.toCompletedChallenges() }
-//                    )
-//                }
-//        }
-//    }
-//    val completedChallenges = getCompletedChallengesUseCase("matt c").cachedIn(viewModelScope)
-//        .map { pagingData ->
-//            pagingData.map { it.toCompletedChallenges() }
-//        }
     val completedChallenges: Flow<PagingData<CompletedChallenges>> =
         getCompletedChallengesUseCase(TEST_USERNAME).cachedIn(viewModelScope)
             .map { pagingData ->
                 pagingData.map { it.toCompletedChallenges() }
-            }
+            }.distinctUntilChanged()
 
 }
 
--- app/src/main/java/com/agesadev/codewarstwo/presentation/screens/challengelist/CompletedChallengesScreen.kt
+++ app/src/main/java/com/agesadev/codewarstwo/presentation/screens/challengelist/CompletedChallengesScreen.kt
@@ -11,13 +11,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.items
 import com.agesadev.codewarstwo.R
 import com.agesadev.codewarstwo.presentation.navigation.Screen
 import com.agesadev.codewarstwo.presentation.screens.challengelist.components.CompletedChallengeItem
+import com.agesadev.codewarstwo.util.TestTags
 import com.agesadev.codewarstwo.util.TestTags.LAZY_COLUMN_COMPLETED
 
 @Composable
@@ -31,174 +34,71 @@ fun CompletedChallengesScreen(
         modifier = Modifier
             .fillMaxSize()
             .testTag(LAZY_COLUMN_COMPLETED)
+
     ) {
-        items(challenges.itemCount) { index ->
-            challenges[index]?.let { challenge ->
+        when (challenges.loadState.prepend) {
+            is LoadState.Loading -> {
+                item {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                }
+            }
+            is LoadState.Error -> {
+                item {
+                    Text(text = stringResource(id = R.string.unknown_error))
+                }
+            }
+            is LoadState.NotLoading -> Unit
+        }
+        when (challenges.loadState.append) {
+            is LoadState.Loading -> {
+                item {
+                    CircularProgressIndicator()
+                }
+            }
+            is LoadState.Error -> {
+                item {
+                    Text(text = stringResource(id = R.string.unknown_error))
+                }
+            }
+            is LoadState.NotLoading -> Unit
+        }
+        when (challenges.loadState.refresh) {
+            is LoadState.Loading -> {
+                item {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                }
+            }
+            is LoadState.Error -> {
+                item {
+                    Text(text = stringResource(id = R.string.unknown_error))
+                }
+            }
+            is LoadState.NotLoading -> Unit
+        }
+        items(challenges) { challenge ->
+            challenge?.let {
                 CompletedChallengeItem(
-                    modifier = Modifier.testTag("CARD_ITEM_COMPOSABLE"),
+                    modifier = Modifier.testTag(TestTags.LIST_CARD_ITEM),
                     challenges = challenge, onChallengeClicked = {
                         navController.navigate(
                             Screen.ChallengeDetailScreen.route + "/${challenge.id}"
 
                         )
-                    })
-            }
-        }
-        challenges.apply {
-            when {
-                loadState.refresh is LoadState.Loading -> {
-                    item {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .fillMaxHeight(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            CircularProgressIndicator()
-                        }
                     }
-                }
-                loadState.append is LoadState.Loading -> {
-                    item {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .fillMaxHeight(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            CircularProgressIndicator()
-                        }
-                    }
-                }
-                loadState.refresh is LoadState.Error -> {
-                    val e = challenges.loadState.refresh as LoadState.Error
-                    item {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .fillMaxHeight(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(text = stringResource(R.string.an_error_please_try_again))
-                        }
-                    }
-                }
-                loadState.append is LoadState.Error -> {
-                    val e = challenges.loadState.append as LoadState.Error
-                    item {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .fillMaxHeight(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(text = stringResource(R.string.an_error_please_try_again))
-                        }
-                    }
-                }
-            }
-        }
-        challenges.apply {
-            when {
-                loadState.refresh is LoadState.NotLoading && loadState.append.endOfPaginationReached && itemCount < 1 -> {
-                    item {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .fillMaxHeight(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(text = stringResource(id = R.string.unknown_error))
-                        }
-                    }
-                }
-                loadState.refresh is LoadState.NotLoading && loadState.append.endOfPaginationReached && itemCount > 0 -> {
-                    item {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .fillMaxHeight(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Button(onClick = { challenges.retry() }) {
-                                Text(text = stringResource(id = R.string.unknown_error))
-                            }
-                        }
-                    }
-                }
+                )
             }
         }
     }
 }
-////            item {
-//////                Box(
-//////                    modifier = Modifier
-//////                        .fillMaxWidth()
-//////                        .wrapContentWidth(Alignment.CenterHorizontally)
-//////                ) {
-//////                    if (challenges.loadState.refresh is LoadState.Loading) {
-//////                        CircularProgressIndicator()
-//////                    }
-//////                }
-////            }
-//        items(challenges.itemCount) {
-//            challenges[it]?.let { challenge ->
-//                CompletedChallengeItem(challenge, onChallengeClicked = {
-//                    navController.navigate(Screen.ChallengeDetailScreen.route + "/${challenge.id}")
-//                })
-//            }
-//        }
-//        when (challenges.loadState.append) {
-//            is LoadState.Error -> {
-//                val error = challenges.loadState.append as LoadState.Error
-//                item {
-//                    Text(error.error.localizedMessage ?: stringResource(R.string.unknown_error))
-//                }
-//            }
-//            LoadState.Loading -> {
-//                item {
-//                    CircularProgressIndicator()
-//                }
-//            }
-//            is LoadState.NotLoading -> Unit
-//        }
-//        when (challenges.loadState.refresh) {
-//            is LoadState.Error -> {
-//                val error = challenges.loadState.refresh as LoadState.Error
-//                item {
-//                    Text(error.error.localizedMessage ?: stringResource(R.string.unknown_error))
-//                }
-//            }
-//            LoadState.Loading -> {
-//                item {
-//                    CircularProgressIndicator()
-//                }
-//            }
-//            is LoadState.NotLoading -> Unit
-//        }
-//    }
-//}
-//
-//@Composable
-//fun ErrorChallengeItem() {
-//    val context = LocalContext.current
-//    Box(
-//        modifier = Modifier
-//            .fillMaxWidth()
-//            .wrapContentWidth(Alignment.CenterHorizontally)
-//    ) {
-//        Button(onClick = {}) {
-//            Text(text = "Retry")
-//        }
-//    }
-//}
-//
-//@Composable
-//fun LoadingChallengeItem() {
-//    Text(
-//        text = "Waiting to Load Data",
-//        modifier = Modifier
-//            .fillMaxWidth()
-//            .wrapContentWidth(Alignment.CenterHorizontally)
-//    )
-//}
